d04ecd468c2e75c6140b04abf96f391b893735f2
gwynith-godin <gwynith22@gmail.com>
2024-09-12 11:31:31 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
-import Labs from "./Labs" //index.tsx is implied
 
 function App() {
   return (
-    <HashRouter>
-      <div>
-      <Routes>
-          <Route path="/" element={<Navigate to="Labs" />} />
-          <Route path="/Labs/*" element={<Labs />} />
-        </Routes>
-      </div>
-    </HashRouter>
-    
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
