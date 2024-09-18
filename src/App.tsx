8166718adc82a003866d392ca3ed7a6418d39147
gwynith-godin <gwynith22@gmail.com>
2024-09-17 22:26:20 -0400

    adding to a1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import Kanbas from "./Kanbas";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Labs from "./Labs" //index.tsx is implied
 
@@ -9,8 +8,9 @@ function App() {
     <HashRouter>
       <div>
       <Routes>
-          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/" element={<Navigate to="Kanbas"/>}/>
           <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
       </div>
     </HashRouter>
